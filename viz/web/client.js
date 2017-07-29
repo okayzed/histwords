4379d7764f1c69584dacc2097b9eb0b7075fe5a0
juniferd <jyk@jyk>
2017-07-29 13:31:42 -0700

    pip requirements
    table showing similarities

--- viz/web/client.js
+++ viz/web/client.js
@@ -3,9 +3,8 @@
     var textEl = $(".input .inputbox");
     var promise = submitMessage(textEl.val(), function(data, res) {
       console.log("LOADED DATA!", data);
-      $(".results").text(JSON.stringify(data, null, 2));
-
-
+      $("#search_term").text(`You searched for ${data.term}!`).show();
+      makeMatrixByYear(data.results);
     });
 
     promise.fail(function() {
@@ -19,6 +18,25 @@
 
     textEl.val("");
   }
+
+  function makeMatrixByYear(data) {
+    var dataByYear = _.groupBy(data, "year");
+    var $thisTable = $("table#matrix_by_year");
+    $thisTable.empty();
+    
+    for (year in dataByYear) {
+      var arrWords = _.sortBy(dataByYear[year], "similarity").reverse();
+      $thisTable.append("<tr class=\"flex-col\" id=\"year_" + year + "\"></tr>");
+      $thisTable.find("#year_" +year+ "").append("<th>" + year + "</th>");
+      for (var i = 0; i < arrWords.length; i++) {
+        var similarity = Math.floor(arrWords[i]["similarity"] * 100);
+        $thisTable.find("#year_" + year + "")
+          .append("<td style=\"background-color: rgba(64,188,216, " + similarity / 100 + ")\">" +
+            arrWords[i]["word"] + "<br/>" + similarity + "%</td>");
+      }
+    }
+    $thisTable.show();
+  };
 
   function submitMessage(msg, cb) {
     // get the room we are in
