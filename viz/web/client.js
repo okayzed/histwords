b9a765f8441e2c8597c9a7f7e8b7cc3069dfa219
okay <okay@chalk>
2017-07-31 08:23:30 -0700

    its possible to toggle between viewtabs

--- viz/web/client.js
+++ viz/web/client.js
@@ -1,10 +1,19 @@
 (function() {
-  var wordSettings = {
-    frozenWord: null,
-    freezeHighlight: false
-  };
+  var SELECTED_VIEW = "table";
+  var DATA, RES;
 
-  // {{{ TABLE VIEW & CO
+  // {{{ JSON VIEW
+  function makeJsonView(data, res) {
+    $(".results").empty();
+    $(".results").append(
+      $("<pre />").text(JSON.stringify(data, null, 2))
+    );
+
+
+  }
+  // }}}
+
+  // {{{ table view & co
   function makeTableView(data, res) {
     makeMatrixByYear(data.results);
     // highlight words on hover
@@ -22,8 +31,14 @@
 
   function makeMatrixByYear(data) {
     var dataByYear = _.groupBy(data, "year");
-    var $thisTable = $("table#matrix_by_year");
-    $thisTable.empty();
+    var resultEl = $(".results");
+    resultEl.empty();
+
+    var $thisTable = $("<table />")
+      .attr('id', "matrix_by_year")
+      .addClass("flex");
+
+    resultEl.append($thisTable);
 
     for (var year in dataByYear) {
       var arrWords = _.sortBy(dataByYear[year], "similarity").reverse();
@@ -48,6 +63,12 @@
   function unhighlightDrift(word, wordClass) {
     $("table#matrix_by_year").find("td[data-word=\"" + word + "\"]").removeClass(wordClass);
   }
+
+  var wordSettings = {
+    frozenWord: null,
+    freezeHighlight: false
+  };
+
   function freezeHighlightDrift(word) {
     if (wordSettings.freezeHighlight) {
       if (word === wordSettings.frozenWord) {
@@ -67,29 +88,26 @@
   }
   // }}} TABLE VIEW
 
-  function submitTextarea() {
-    var textEl = $(".header .inputbox");
-    var word = textEl.val();
+  // {{{ word cloud plot
+  function makeCloudView(data, res) {
+    $(".results").empty();
+    var resultEl = d3.select(".results")
+      .selectAll("div")
+      .data(data.results)
+      .enter()
+      .append("div")
+        .style("position", "absolute")
+        .style("font-size", function(d) { return parseInt(d.similarity * 30, 10) + "px";})
+        .style("left", function(d) { return (d.position.x * 50 + 1000)+ "px"; })
+        .style("top", function(d) { return (d.position.y * 50 + 650) + "px"; })
+        .style("opacity", function(d) { return (d.year - 1800) / 100 - 0.2; })
+        .text(function(d) { return d.word; })
 
-    textEl.attr("disabled", true);
-    textEl.attr("placeholder", "searching for: " + word);
-
-    var visualize = makeTableView;
-    var promise = submitMessage(word, visualize);
-
-    promise.fail(function() {
-      promise.loadingcube.css("background-color", "#fdd");
-    });
-
-    promise.done(function() {
-      textEl.attr("disabled", false);
-      textEl.attr("placeholder", "enter a search term");
-    });
-
-
-    textEl.val("");
   }
 
+  // }}}
+
+  // {{{ server communications
   function submitMessage(msg, cb) {
     // get the room we are in
     return getCmd("search", { term: msg }, cb);
@@ -136,7 +154,50 @@
     return ret;
 
   }
+  // }}} server comm
 
+  // {{{
+  var VIZ = {
+    "table" : makeTableView,
+    "cloud" : makeCloudView,
+    "json" : makeJsonView
+  };
+
+  function visualizeResults(data, res) {
+    DATA = data;
+    RES = res;
+
+    drawVisuals();
+  }
+
+  function drawVisuals() {
+    var visualize = VIZ[SELECTED_VIEW];
+    visualize(DATA, RES);
+  }
+  // }}}
+
+  // {{{ text area handler
+  function submitTextarea() {
+    var textEl = $(".header .inputbox");
+    var word = textEl.val();
+
+    textEl.attr("disabled", true);
+    textEl.attr("placeholder", "searching for: " + word);
+
+    var promise = submitMessage(word, visualizeResults);
+
+    promise.fail(function() {
+      promise.loadingcube.css("background-color", "#fdd");
+    });
+
+    promise.done(function() {
+      textEl.attr("disabled", false);
+      textEl.attr("placeholder", "enter a search term");
+    });
+
+
+    textEl.val("");
+  }
 
   // Gets ready to press enter
   function handleKeyDown(event) {
@@ -147,6 +208,29 @@
 
   $(function() {
     $(".header .inputbox").on("keydown", handleKeyDown);
+    viewtabBoxEl = $(".viewtab_box");
+    _.each(VIZ, function(func, viz) {
+      console.log("VIZ", viz);
+      var el = $("<div class='viewtab' />");
+      el.text(viz)
+        .data("viz", viz);
+
+      if (viz == SELECTED_VIEW) {
+        el.addClass("active");
+      }
+
+      viewtabBoxEl.append(el);
+
+      // click handler for viewtab
+      el.on("click", function(el) {
+        SELECTED_VIEW = $(this).data("viz");
+        $(".viewtab").removeClass("active");
+        $(this).addClass("active");
+        drawVisuals();
+      });
+    });
+
   });
+  // }}} text area handler
 
 })();
